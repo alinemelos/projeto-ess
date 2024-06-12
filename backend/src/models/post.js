ba2bd4a1bcac75576da7349150495244972bb45c
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-06-12 11:52:45 -0300

    feat: criacao model post

---
+++ backend/src/models/post.js
@@ -0,0 +1,18 @@
+const { v4: uuidv4 } = require('uuid');
+
+class Post {
+    constructor(user_id, filme_id, nota, review, comments = []) {
+        this.post_id = uuidv4(); // Gerar automaticamente um UUID
+        this.user_id = user_id;
+        this.filme_id = filme_id;
+        this.nota = nota;
+        this.review = review;
+        this.comments = comments;
+    }
+
+    validateNota() {
+        return this.nota >= 0 && this.nota <= 5;
+    }
+}
+
+module.exports = Post;
