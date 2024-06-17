ab7c65dea87e876dfd8ec441a4efe92d57a79e01
Ariel Santos <adtr.santos@gmail.com>
2024-06-17 04:58:20 -0300

    fix: now generating UUID

--- backend/src/models/movies.js
+++ backend/src/models/movies.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 class Movie {
     constructor(filme_id, nome, ano, duracao, genero, sinopse = [], imagem, plataformas = []) {
         this.filme_id = uuidv4(); // Gerar automaticamente um UUID
-        this.filme_id = filme_id;
+        //this.filme_id = filme_id;
         this.nome = nome;
         this.ano = ano;
         this.duracao = duracao;
