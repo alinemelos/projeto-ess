c29b26100e03936a5592518ac76d2b835e646a3a
Ariel Santos <adtr.santos@gmail.com>
2024-06-17 23:03:13 -0300

    feat: addContent || Done

--- backend/tests/content.test.js
+++ backend/tests/content.test.js
@@ -17,7 +17,7 @@ defineFeature(content_feature, (test) => {
         and(/^Preencho as informações "(.*)", "(.*)", "(.*)", "(.*)", "(.*)" e "(.*)" com os dados "(.*)", "(.*)", "(.*)", "(.*)", "(.*)" e "(.*)" respectivamente$/, async (arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11) => {
             const content_test = {
                 "nome": arg6,
-                "ano": Number(arg7),
+                "ano": arg7,
                 "duracao": arg8,
                 "genero": arg9,
                 "sinopse": arg10,
@@ -32,19 +32,18 @@ defineFeature(content_feature, (test) => {
         });
 
         then(/^Aparece uma mensagem de confirmação "(.*)" e posso ver o filme na "(.*)".$/, async(arg0, arg1) => {
-            //const response = await axios.get('http://localhost:3000');
-            //const filmes = response.data.find(f => f.filme_id === filme_id);
-            //const filme = filmes.find(f => f.filme_id === filme_id);
+            const response = await axios.get('http://localhost:3000');
+            const filme = response.data.find(filme => filme.filme_id === filme_id);
 
 
-            // expect(filmes).toEqual(expect.objectContaining({ 
-            //     "nome": "Barbie",
-            //     "ano": Number(2023),
-            //     "duracao": "1h54m",
-            //     "genero":  Number(2),
-            //     "sinopse": "Barbie parte para o mundo humano em busca da verdadeira felicidade.",
-            //     "poster": "https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg"
-            // }));
+            expect(filme).toEqual(expect.objectContaining({ 
+                "nome": "Barbie",
+                "ano": "2023",
+                "duracao": "1h54m",
+                "genero":  "2",
+                "sinopse": "Barbie parte para o mundo humano em busca da verdadeira felicidade.",
+                "poster": "https://image.tmdb.org/t/p/original/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg"
+            }));
         });
     });
 });
