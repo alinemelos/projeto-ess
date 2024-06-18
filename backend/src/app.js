4e1cce4c0a08785662e2eb82335ebf3b0f859f8d
Aline Melo <meloaline703@gamil.com>
2024-06-18 10:49:21 -0300

    fix (mainteance): fixing branches

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
-const router = require("./views/router");
+const router = require("./views/exemplo.routes");
+const postRouter = require("./views/posts.routes");
+const maintenanceRouter = require("./views/maintenance.routes");
 
 app.use(express.json()); // Add this line to parse JSON request bodies
 
 app.use("/", router);
+
+// Miguel
+app.use("/posts", postRouter);
+
+//Aline
+app.use("/maintenance", maintenanceRouter);
+
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
 });
