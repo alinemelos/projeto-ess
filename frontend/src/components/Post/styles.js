bd52e141478edd06b82d8a9e83468bef0a75206f
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-01 16:04:30 -0300

    feat: forum components

---
+++ frontend/src/components/Post/styles.js
@@ -0,0 +1,42 @@
+const styles = {
+    container: {
+        borderRadius: '8px',
+        marginBottom: '1.2rem',
+        width: '100%',
+    },
+    post__header: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: '12px',
+    },
+    post__header__data: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    post__header__data_userID: {
+        marginRight: '1rem',
+    },
+    post__header__options: {
+        marginLeft: 'auto',
+    },
+    post__body: {
+        marginBottom: '12px',
+    },
+    post__body__content_review: {
+        fontSize: '16px',
+        lineHeight: '1.5',
+    },
+    post__footer: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginLeft: '4rem',
+    },
+    post__footer_arrow: {
+        marginLeft: "auto",
+    }
+  };
+  
+  export default styles;
+  
