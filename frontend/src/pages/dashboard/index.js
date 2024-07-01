af13201f655c69660c4e33f4d11c5f782f7ab70e
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-01 16:04:43 -0300

    feat: rendering movies in dashboard

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from "react-router-dom";
 import Button from '@mui/material/Button';
-import StarsRating from '../../components/StarsRating';
 import styles from './styles';
 import CreatePost from '../../services/posts/CreatePost';
+import GetFilmes from '../../services/filmes/GetFilmes';
 
 const Dashboard = () => {
     const navigate = useNavigate();
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => { 
+        async function fetchData() {
+            const response = await GetFilmes();
+            setFilmes(response.data);
+        }
+        fetchData();
+    }, []);
 
     const handleBack = () => {
         navigate("/")
@@ -21,13 +30,17 @@ const Dashboard = () => {
         <div style={styles.container}>
             <div>
                 <h1>Bem vindo ao dashboard</h1>
-                <StarsRating/>
                 <Button variant="contained" color="primary" onClick={handleBack}>
                     Voltar
                 </Button>
-                <Button variant="contained" color="primary" onClick={handlePost}>
-                    Criar post
-                </Button>
+                {filmes.map((filme) => (
+                    <Link to={`/filme/${filme.filme_id}`} key={filme.id} style={{ textDecoration: 'none', color: 'inherit' }}>
+                        <div>
+                            <h2>{filme.nome}</h2>
+                            <p>{filme.sinopse}</p>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </div>
     );
