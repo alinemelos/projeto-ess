cfe978ca0312393462f4cd64c0129feea68d7efd
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-01 16:05:02 -0300

    feat: filme component

---
+++ frontend/src/pages/filme/styles.js
@@ -0,0 +1,26 @@
+// src/pages/FilmStyles.js
+const styles = {
+    container: {
+      maxWidth: '80%',
+      margin: '0 auto 0 7%',
+      padding: '20px',
+      backgroundColor: '#fff',
+    },
+    title: {
+      fontSize: '2em',
+      marginBottom: '20px',
+      color: '#333',
+    },
+    synopsis: {
+      fontSize: '1.2em',
+      color: '#555',
+    },
+    detail: {
+      marginTop: '20px',
+      fontSize: '1em',
+      color: '#777',
+    },
+  };
+  
+export default styles;
+  
