a11c64dd5411ae6b6b7b776e474f5bf93fc3f879
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-04 14:36:01 -0300

    feat: remove post

--- frontend/src/services/posts/DeletePost.js
+++ frontend/src/services/posts/DeletePost.js
@@ -1,15 +1,19 @@
 import api from '../index'
 
-export default async function DeletePost(post_id, userd_id, filme_id) {
+export default async function DeletePost(post_id, user_id, filme_id) {
   try {
     const data = {
       post_id: post_id,
-      user_id: userd_id,
+      user_id: user_id,
       filme_id: filme_id
     }
-    const response = await api.delete('/posts', data)
+    const response = await api.delete('/posts', { data: data })
     return response
   } catch (error) {
-    console.error(error)
+    if (error.response && error.response.data && error.response.data.error === 'Post not found.') {
+      return 'Post not found.'
+    } else {
+      return 'Error'
+    }
   }
 }
