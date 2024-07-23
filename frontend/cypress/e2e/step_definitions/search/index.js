ca1848cb8f1a77c8ddfc616ffedc4208f66088d4
Miguel-Oliveiraa <miguellgomes64@gmail.com>
2024-07-23 13:44:54 -0300

    fix: cypress test

--- frontend/cypress/e2e/step_definitions/search/index.js
+++ frontend/cypress/e2e/step_definitions/search/index.js
@@ -1,39 +1,36 @@
 import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
 
+// Scenario: Busca de filme por nome
 
-  // Scenario: Busca de filme por nome
-  
-  let busca = ''
-  Given('que o usuário está na página inicial', () => {
-    cy.visit(`http://localhost:3005/userdashboard`)
-  })
+let busca = ''
+Given('que o usuário está na página inicial', () => {
+  cy.visit(`http://localhost:3005/userdashboard`)
+})
 
-  When('o usuário seleciona a opção de pesquisa', () => {
-    busca = cy.get('[data-testid="caixa de busca"]')
-  })
+When('o usuário seleciona a opção de pesquisa', () => {
+  busca = cy.get('[data-testid="caixa de busca"]')
+})
 
-  And('o usuário escreve "Vingadores" e clica no botão "Pesquisar"', () => {
-    busca.type('Vingadores{enter}')
-  })
-  
-  Then('os posts dos filmes "Os Vingadores" e "Vingadores Ultimato" são exibidos', () => {
-  })
+And('o usuário escreve "Vingadores" e clica no botão "Pesquisar"', () => {
+  busca.type('Vingadores{enter}')
+})
 
+Then('os posts dos filmes "Os Vingadores" e "Vingadores Ultimato" são exibidos', () => {
+  cy.get('span').contains('Os Vingadores').should('exist')
+})
 
-  // Scenario: Busca de filme por categoria
+// Scenario: Busca de filme por categoria
 
-  And('o usuário escreve "Ação" e clica no botão "Pesquisar"', () => {
-    busca.type('Ação{enter}')
-  })
-  
-  Then('os posts dos filmes da categoria "Ação" são exibidos', () => {})
+And('o usuário escreve "Ação" e clica no botão "Pesquisar"', () => {
+  busca.type('Ação{enter}')
+})
 
+Then('os posts dos filmes da categoria "Ação" são exibidos', () => {})
 
-  // Scenario: Busca falhou
+// Scenario: Busca falhou
 
-  And('o usuário escreve "asdfghjkl" e clica no botão "Pesquisar"', () => {
-    busca.type('asdfghjkl{enter}')
-  })
-  
-  Then('uma mensagem informando que não foram encontrados resultados é exibida', () => {
-  })
+And('o usuário escreve "asdfghjkl" e clica no botão "Pesquisar"', () => {
+  busca.type('asdfghjkl{enter}')
+})
+
+Then('uma mensagem informando que não foram encontrados resultados é exibida', () => {})
