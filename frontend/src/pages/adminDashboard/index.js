fed359a8d8ed00e21a375c2cca9d1c7e7a833c11
Miguel Oliveira <55458349+Miguel-Oliveiraa@users.noreply.github.com>
2024-07-22 21:54:17 -0300

    Revert "feat(Search) implemented"

--- frontend/src/pages/adminDashboard/index.js
+++ frontend/src/pages/adminDashboard/index.js
@@ -6,25 +6,18 @@ import Header from '../../components/ContentComponents/Header'
 
 const AdminDashboard = () => {
   const [showContent, setShowContent] = useState(false)
-  const [filmes_novos, setfilmes] = useState('')
-
-  function retornar_filmes(result) {
-    setfilmes(result)
-  }
-
   function handleContent() {
     setShowContent(!showContent)
   }
-
   function ModalAddMovie(props) {
     return <>{props.showContent && <ModalContentAdd showContent={showContent} handleContent={handleContent} />}</>
   }
 
   return (
     <div style={styles.background}>
-      <Header topRightName={'ADM'} retornar_filmes={retornar_filmes} />
+      <Header topRightName={'ADM'} />
       <div>
-        <CarouselMovies filmes_novos={filmes_novos} handleContent={handleContent} showMovieFrame={true} />
+        <CarouselMovies handleContent={handleContent} showMovieFrame={true} />
         {showContent && <ModalAddMovie showContent={showContent} />}
       </div>
     </div>
